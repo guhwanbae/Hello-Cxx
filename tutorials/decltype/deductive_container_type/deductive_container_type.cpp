3f5a4db0209c9274a9eda206b2e316a920ab73b8
Gu-hwan Bae <guhwan.bae@gmail.com>
2018-08-13 21:53:01 +0900

    Infer the type of container elements. And type check by using compiler error message.

--- tutorials/decltype/deductive_container_type/deductive_container_type.cpp
+++ tutorials/decltype/deductive_container_type/deductive_container_type.cpp
@@ -6,6 +6,10 @@
 #include <iostream>
 #include <vector>
 
+// Type checker by using compiling error message.
+template <typename T>
+class TypeCheck;
+
 // C++14
 template <typename Container, typename Index>
 decltype(auto)
@@ -25,16 +29,18 @@ element_cxx11(Container&& c, Index i)
 
 int main() {
     std::vector<int> iv(5, 1);
-    // Return type is int.
-    std::cout << element_cxx14(iv, 3) << std::endl;
+    // Return type is int &.
+    TypeCheck<decltype(element_cxx14(iv, 3))> ivType;
 
-    // Return type is const std::string.
+    // Return type is const std::string (std::basic_string<char> &).
     std::vector<const std::string> sv {"fox", "rabbit", "lamb", "dog"};
-    std::cout << element_cxx11(sv, 0) << std::endl;
+    TypeCheck<decltype(element_cxx11(sv, 2))> svType;
 
-    const double palomenso {"0.1125"};
+    const double palomenso {0.1125};
     auto x {palomenso}; // Type of x is double.
+    TypeCheck<decltype(x)> xType;
     decltype(auto) y {palomenso};   // Type of y is const double.
+    TypeCheck<decltype(y)> yType;
 
     return 0;
 }
