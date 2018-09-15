788427b490d4686bf65b6d087d2ff2f86eb74a31
Gu-hwan Bae <guhwan.bae@gmail.com>
2018-09-16 01:09:34 +0900

    Measurement performance of parallel_accumulate on linux system(Quad core)

--- tutorials/accumulate/parallel_accumulate/run_and_log_measurement.sh
+++ tutorials/accumulate/parallel_accumulate/run_and_log_measurement.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
-echo 'System information :'
+echo '[System information]\n'
 uname -a
+echo '\n'
 
 echo '----------------------Optimized program performance----------------------'
 ./parallel_accumulate_optimized -s
+echo '\n'
 ./parallel_accumulate_optimized -p
+echo '\n'
 
 echo '----------------------Unoptimized program performance----------------------'
 ./parallel_accumulate_unoptimized -s
+echo '\n'
 ./parallel_accumulate_unoptimized -p
+echo '\n'
 
